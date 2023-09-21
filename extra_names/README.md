70850b981a3615c71502051371e0c6c0e414f3be
Charlie Ross <charlie@matx.ai>
2023-09-21 15:48:40 -0700

    Include bsc2bsv in extra_names example.

--- extra_names/README.md
+++ extra_names/README.md
@@ -14,4 +14,12 @@ Interestingly, `mkThingWithTooFewNames` does generate an error, as expected, but
 the error message is just `Prelude.!!: index too large%` which is not very
 helpful, and contains no line numbers or even filenames.
 
+Because it is interesting, I also run bsc2bsv for both examples. For
+mkThingWithExtraNames, the extra name is ignored... but the generated code still
+does not compile, for what looks like unrelated reasons.
+
+For mkThingWithTooFewNames, the interface method is declared with only one
+argument, which is an error.   If you fix that error manually, then it fails
+with the same error as mkThingWithExtraNames.
+
 To replicate, just execute `./compile.sh` in this dir.
--- extra_names/compile.sh
+++ extra_names/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 ../clean.sh
+rm -fv code/*.bsv # bsc2bsv will create these.
 . ../functions.sh
 
 path_flags="$(path_dir_flags ./code)"
@@ -11,6 +12,8 @@ expected_outputs=(./code/ThingWithExtraNames.bo)
 expected_outputs+=(./code/mkThingWithExtraNames.v)
 BSC $path_flags -verilog -g mkThingWithExtraNames ./code/ThingWithExtraNames.bs
 
+set +e # The rest will all fail to compile, but we want to keep going.
+
 # This doesn't compile, but the error it generates is poor.
 source_files=(./code/ThingWithTooFewNames.bs)
 expected_outputs=(./code/ThingWithTooFewNames.bo)
@@ -18,3 +21,21 @@ expected_outputs+=(./code/mkThingWithTooFewNames.v)
 BSC $path_flags -verilog -g mkThingWithTooFewNames ./code/ThingWithTooFewNames.bs
 
 # Ideally both would not compile, and give good error messages.
+
+# Just for fun, do the same with bsv:
+bsc2bsv code/ThingWithExtraNames.bs >code/ThingWithExtraNames.bsv
+bsc2bsv code/ThingWithTooFewNames.bs >code/ThingWithTooFewNames.bsv
+
+# This fails to compile with some unrelated (I think?) error.
+source_files=(./code/ThingWithExtraNames.bsv)
+expected_outputs=(./code/ThingWithExtraNames.bo)
+expected_outputs+=(./code/mkThingWithExtraNames.v)
+BSC $path_flags -verilog -g mkThingWithExtraNames ./code/ThingWithExtraNames.bsv
+
+# This doesn't compile, because the interface declaration has a method with one
+# argument, but the implementation is for a method with two arguments.  (If you
+# fix that manually, you end up with the same error as the previous case.)
+source_files=(./code/ThingWithTooFewNames.bsv)
+expected_outputs=(./code/ThingWithTooFewNames.bo)
+expected_outputs+=(./code/mkThingWithTooFewNames.v)
+BSC $path_flags -verilog -g mkThingWithTooFewNames ./code/ThingWithTooFewNames.bsv
